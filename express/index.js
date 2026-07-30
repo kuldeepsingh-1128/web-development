1d2655df017304add5ac55852dff41af94695e3b
kuldeepsingh-1128 <28kuldeep1104@gmail.com>
2026-07-30 14:42:25 +0530

    Express

---
+++ express/index.js
@@ -0,0 +1,50 @@
+const express=require("express");
+const app =express();
+
+// console.dir(app);
+
+let port=8080;
+
+app.listen(port,()=>{
+    console.log(`app listing at port: ${port}`);
+})
+
+app.get("/",(req,res)=>{
+    res.send("hello,i am root");
+    console.log("you contected home page")
+})
+// app.get("/search",(req,res)=>{
+//     res.send("thank u for searching");
+//     console.log("thank u for searching");
+// })
+// app.get("/apple",(req,res)=>{
+//     res.send("you contected apple page");
+//     console.log("you contected apple page")
+// })
+// app.use((req,res)=>{
+//     res.status(404).send("page not found..");
+// })
+
+app.get("/:username/:id",(req,res)=>{
+    console.log(req.params);
+    let {username,id}=req.params;
+    res.send(`wellcome to the page of @${username}`);
+})
+
+// app.use((req,res)=>{
+//     console.log("Request recived..");
+//     // res.send("this is a basic response..");
+//     // res.send({
+//     //     "name":"kuldeep",
+//     //     "study":"Btech"
+//     // });
+//     res.send("<h1>html response</h1>");
+// })
+
+app.get("/search",(req,res)=>{
+    let {q}=(req.query);
+    if(!q){
+        res.send("you not search for anything..");
+    }
+    res.send(`these are the search result for query=${q}`);
+})
