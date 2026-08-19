ba0f05ff3d2627f300b8a7bc33e3ff1de59b6617
kuldeepsingh-1128 <28kuldeep1104@gmail.com>
2026-08-19 22:24:45 +0530

    mini whatsapp project using database

--- Mongodb with Express/index.js
+++ Mongodb with Express/index.js
@@ -7,9 +7,13 @@ const path=require("path");
 
 const chat=require("./models/chat.js");
 
+const methodOverride=require("method-override");
+app.use(methodOverride("_method"));
+
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended:true}));
 
 main().then(()=>{
     console.log("connection success");
@@ -29,7 +33,53 @@ app.get("/",(req,res)=>{
     res.send("root working..")
 })
 
+//new route
+app.get("/chats/new" ,(req,res)=>{
+    res.render("newchat.ejs");
+})
 
+//create route
+app.post("/chats",(req,res)=>{
+    let {from,to,msj}=req.body;
+    let newchat=new chat({
+        from:from,
+        to:to,
+        msj:msj,
+        created_at:new Date()
+    })
+    // console.log(newchat);
+    newchat.save().then((res)=>{
+        console.log("successfull created new chat");
+    }).catch((err)=>{
+        console.log(err);
+    })
+    res.redirect("/chats");
+});
+
+//edit route
+app.get("/chats/:id/edit",async(req,res)=>{
+    let {id}=req.params;
+    let Chat=await chat.findById(id);
+    res.render("edit.ejs" ,{Chat});
+})
+
+//update route
+app.put("/chats/:id",async (req,res)=>{
+    let {id}=req.params;
+    let {msj:newmsj}=req.body;
+    let updatemsj=await chat.findByIdAndUpdate(id,
+        {msj:newmsj} ,
+        {runValidators:true, new:true});
+    res.redirect("/chats");
+})
+
+//delete
+app.delete("/chats/:id",async(req,res)=>{
+    let {id}=req.params;
+    let deletedchat=await chat.findByIdAndDelete(id);
+    console.log(deletedchat);
+    res.redirect("/chats");
+})
 
 // let chat1=new chat({
 //     from:"rohan",
@@ -45,6 +95,5 @@ app.get("/",(req,res)=>{
 
 app.get("/chats",async (req,res)=>{
     let chats=await chat.find();
-    console.log(chats);
     res.render("index.ejs",{chats});
 })
